6ab1ece75b9a9094f634a69dffbb00cf8baf1eee
manuelfloresay <manuelflores.a.y@gmail.com>
2024-05-11 21:34:44 -0400

    feat: removed unused statement and modules

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
 import uvicorn
 from routers import prompt
-from models import Request, Response
 
 app = FastAPI()
-
 app.include_router(prompt)
-app.state.limiter = limiter
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
