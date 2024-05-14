73918d9c9e27e3bf30148331f0dffa9e2a15bf12
manuelfloresay <manuelflores.a.y@gmail.com>
2024-05-14 15:27:24 -0400

    feat: added missing library

--- service/init_chatbot.py
+++ service/init_chatbot.py
@@ -15,9 +15,9 @@ def init_chatbot():
     logging.info("Connecting to LLM model in AWS")
     model_parameter = {"temperature": float(os.environ.get("TEMPERATURE")), "top_p": float(os.environ.get("TOP_P")), "max_tokens_to_sample": int(os.environ.get("MAX_TOKENS_TO_SAMPLE"))}
     boto3_bedrock = boto3.client(service_name='bedrock-runtime', region_name=os.environ.get("LLM_AWS_REGION"))
-    llm = Bedrock(model_id=os.environ.get("LLM_MODEL_ID"), client=boto3_bedrock, model_kwargs=model_parameter)
+    llm = Bedrock(model_id=os.environ.get("LLM_MODEL_ID"), client=boto3_bedrock, model_kwargs=model_parameter, region_name=os.environ.get("LLM_AWS_REGION"))
 
-    react_agent_llm = Bedrock(model_id=os.environ.get("LLM_MODEL_ID"), model_kwargs=model_parameter)
+    react_agent_llm = Bedrock(model_id=os.environ.get("LLM_MODEL_ID"), model_kwargs=model_parameter, region_name=os.environ.get("LLM_AWS_REGION"))
     tools = load_tools(["wikipedia"], llm=react_agent_llm)
     tools.append(Tool.from_function(
             name="get_item_from_dynamodb",
